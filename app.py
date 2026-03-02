6b819492eff2de8e73e848c7d5fefc0d73931d96
eduardburin1994 <burinedward@gmail.com>
2026-03-02 19:46:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 # Настройка логирования
 logging.basicConfig(
-    format='%(asime).19s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
