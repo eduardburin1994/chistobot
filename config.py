6d8b07b41847654df16a3f4c62d3d3be2d4db8ac
Eduard Burin <eduardburin1994@gmail.com>
2026-03-05 10:16:54 +0300

    F

--- config.py
+++ config.py
@@ -2,49 +2,26 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные из .env файла
+# Загружаем переменные из .env файла (В САМОМ НАЧАЛЕ!)
 load_dotenv()
 
-# Токен бота - теперь читается из .env
-TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', '8730099509:AAF83M1EjAqwB7FErvaRXJUPKaP-1kREv8I')
-import os
-import logging
-from telegram.warnings import PTBUserWarning
-import warnings
-import database as db
+# Токен бота - читаем из .env через os.getenv()
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
-# Токен бота - читаем из переменной окружения (ВАЖНО: имя должно совпадать с Render!)
-TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '8730099509:AAF83M1EjAqwB7FErvaRXJUPKaP-1kREv8I')
-
-# Пароль для курьеров (можно вынести в переменные окружения)
-COURIER_PASSWORD = os.environ.get('COURIER_PASSWORD', 'courier123')
+# Пароль для курьеров
+COURIER_PASSWORD = os.getenv('COURIER_PASSWORD', 'courier123')
 
 # ID главного администратора
 MAIN_ADMIN_ID = 954653245
 
-# Настройки ЮKassa (потом настроим)
-YOOKASSA_SHOP_ID = os.environ.get('YOOKASSA_SHOP_ID', '')
-YOOKASSA_SECRET_KEY = os.environ.get('YOOKASSA_SECRET_KEY', '')
+# Настройки ЮKassa
+YOOKASSA_SHOP_ID = os.getenv('YOOKASSA_SHOP_ID', '')
+YOOKASSA_SECRET_KEY = os.getenv('YOOKASSA_SECRET_KEY', '')
 YOOKASSA_RETURN_URL = 'https://t.me/ваш_бот'
 
-# Данные администраторов
-admin_data = {
-    'prices': db.load_prices(),  # ← загружаем из БД
-    'admins': [MAIN_ADMIN_ID],
-    'blacklist': [],
-    'blocked_users': [],
-    'action_log': [],
-    'test_mode': False
-}
-
-# Время работы бота (можно менять через админку)
-WORK_HOURS = {
-    'start': 10,  # 10:00
-    'end': 22     # 22:00
-}
-
-# Временное хранение данных заявки
-user_data = {}
+import logging
+import warnings
+from telegram.warnings import PTBUserWarning
 
 # Настройка логирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -53,3 +30,25 @@ logger = logging.getLogger(__name__)
 # Отключаем предупреждения
 warnings.filterwarnings("ignore", message="If 'per_message=False'", category=PTBUserWarning)
 logging.getLogger("httpx").setLevel(logging.WARNING)
+
+# Импортируем базу данных ПОСЛЕ загрузки переменных
+import database as db
+
+# Данные администраторов
+admin_data = {
+    'prices': db.load_prices(),
+    'admins': [MAIN_ADMIN_ID],
+    'blacklist': [],
+    'blocked_users': [],
+    'action_log': [],
+    'test_mode': False
+}
+
+# Время работы бота
+WORK_HOURS = {
+    'start': 10,
+    'end': 22
+}
+
+# Временное хранение данных заявки
+user_data = {}
